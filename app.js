37f82047b5721545133cf8171037134eebb9be3f
Aaron Zentli <aaron.zentli@gmail.com>
2019-02-26 17:32:50 -0600

    Archivo de la aplicación, se inicializan variables

---
+++ app.js
@@ -0,0 +1,19 @@
+const express 		= require('express');
+const logger 	    = require('morgan');
+const bodyParser 	= require('body-parser');
+const passport      = require('passport');
+const pe            = require('parse-error');
+const cors          = require('cors');
+
+const v1    = require('./routes/v1');
+const app   = express();
+
+const CONFIG = require('./config/config');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+
+//Log Entorno
+console.log("Entorno:", CONFIG.app);
