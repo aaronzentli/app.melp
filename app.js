c9f734055d273434cffb1e770f04fc51260a6e6a
Aaron Zentli <aaron.zentli@gmail.com>
2019-02-26 17:37:12 -0600

    Tratamientos de posibles errores

--- app.js
+++ app.js
@@ -42,3 +42,23 @@ app.use('/', function(req, res){
 	res.json({status:"success", message:"Melp Restaurant API", data:{}})
 });
 
+// Tratar errores 
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+process.on('unhandledRejection', error => {
+    console.error('Error', pe(error));
+});
